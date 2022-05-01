11fa9875d72716c354744745e7259091c6fe8a57
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-01 14:07:32 +0200

    Aggiunto select dinamico

--- js/script-migliorato.js
+++ js/script-migliorato.js
@@ -2,8 +2,11 @@
     VARIABILI GLOBALI
 ***************************/
 const stampaQui = document.querySelector(".main-col");
+const seleziona = document.getElementById("filter");
 const hexLimit = 6;
 const alphaNum = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const optionArray = [];
+
 
 
 /***************************
@@ -12,7 +15,6 @@ const alphaNum = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const animalArray = dataArray.filter( (el) => el.type === "animal" );
 const vegetalArray = dataArray.filter( (el) => el.type === "vegetable" );
 const humanArray = dataArray.filter( (el) => el.type === "user" );
-
 
 //  ---> COLORE
     // Tutto questo codice non è farina del mio sacco. E' qui solo perché possa studiarlo.
@@ -32,12 +34,12 @@ function coloreCasuale(){
 //  ---> STAMPA
 function stampaSchede (arr) {
     arr.forEach(el => {
-        const {name, prefix, type, color} = el;
-        stampaScheda(name, prefix, type, color);
+        const {name, prefix, type} = el;
+        stampaScheda(name, prefix, type);
     })
 }
 
-function stampaScheda(name, prefix, type, color){
+function stampaScheda(name, prefix, type){
     stampaQui.innerHTML +=
     `
     <div class="my-card ${type}">
@@ -48,22 +50,42 @@ function stampaScheda(name, prefix, type, color){
 }
 
 function stampaSelezionato(){
-    document.getElementById("filter").addEventListener("change", function(){
+    seleziona.addEventListener("change", function(){
         if(this.value == 0){
             stampaQui.innerHTML = "";
-            stampaSchede(dataArray);
+            stampaSchede(animalArray);
         } else if(this.value == 1){
             stampaQui.innerHTML = "";
-            stampaSchede(animalArray);
+            stampaSchede(vegetalArray);
         } else if(this.value == 2){
             stampaQui.innerHTML = "";
-            stampaSchede(vegetalArray);
-        } else if(this.value == 3){
-            stampaQui.innerHTML = "";
             stampaSchede(humanArray);
+        } else{
+            stampaQui.innerHTML = "";
+            stampaSchede(dataArray);
         }
     })
 }
+
+//  ---> SELECT DINAMICO
+// Creare una funzione; che mi estrapoli da un array di oggetti; non gli oggetti che posseggono una caratteristica, ma la caratteristica stessa.
+
+function estrapolaOpzioni(){
+    dataArray.forEach((el) => {
+        if(!optionArray.includes(el.type)){
+            optionArray.push(el.type);
+        }
+    })
+}
+
+function stampaOpzioni(){
+    for(let i=0; i<optionArray.length; i++){
+        seleziona.innerHTML += 
+        `<option id="${optionArray[i]}" value="${i}">${optionArray[i]}</option>`
+        console.log(i);
+    }
+}
+
 
 
 /***************************
@@ -71,4 +93,5 @@ function stampaSelezionato(){
 ***************************/
 stampaSchede(dataArray);
 stampaSelezionato();
-
+estrapolaOpzioni();
+stampaOpzioni();
